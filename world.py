98eb838c61201605e496a37620dc4e52f8586bef
George Grasham <geoed2000@gmail.com>
2021-02-26 17:33:06 +0000

    updated world.py to be nicer, load faster, and take a scheduel when simulating

--- world.py
+++ world.py
@@ -1,5 +1,6 @@
 from random import shuffle, random
 import os
+from typing import Optional
 
 import car
 import intersection
@@ -11,18 +12,19 @@ class World:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-        self.streets: list[street.Street] = []
+        self.streets: dict[str, street.Street] = []
         self.cars: list[car.Car] = []
 
         self.duration: int = 0
         self.points_per_car: int = 0
-        self.intersections: dict[int, intersection.Intersection] = {}
+        self.intersections: list[Optional[intersection.Intersection]]
 
         print('Generating world')
         with open(f'input/{filename}', 'r') as file:
             first_line_data = file.readline().strip('\n').split(' ')
             self.duration = int(first_line_data[0])
-            # intersection_count = int(first_line_data[1])
+            self.intersection_count = int(first_line_data[1])
+            self.intersections = [None for i in range(self.intersection_count)]
             street_count = int(first_line_data[2])
             car_count = int(first_line_data[3])
             self.points_per_car = int(first_line_data[4])
@@ -31,10 +33,10 @@ class World:
             for _ in range(street_count):
                 street_data = file.readline().strip('\n').split(' ')
                 street_object = street.Street(street_data[2], int(street_data[3]), self)
-                self.streets.append(street_object)
+                self.streets[street_object.name] = street_object
 
                 intersection_id = int(street_data[1])
-                if intersection_id not in self.intersections:
+                if not(self.intersections[intersection_id]):
                     self.intersections[intersection_id] = intersection.Intersection(intersection_id)
                 self.intersections[intersection_id].streets.append(street_object)
 
@@ -43,15 +45,17 @@ class World:
                 car_data = file.readline().strip('\n').split(' ')
                 route: list[street.Street] = []
                 for route_street in car_data[1:]:
-                    street_object = [s for s in self.streets if s.name == route_street][0]
+                    street_object = self.streets[route_street]
                     route.append(street_object)
                 self.cars.append(car.Car(route, route[0].length + i))
 
-        self.descend()
-
-    def simulate(self) -> int:
+    def simulate(self, scheduel: list[list[str]]) -> int:
         print('-> Duplicating Cars and Intersections')
         intersections = self.intersections.copy()
+
+        for i, sched in enumerate(scheduel):
+            intersections[i].schedule = sched
+
 
         points = 0
 
