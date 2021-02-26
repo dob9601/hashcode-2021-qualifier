ef5fceedb94d6e76f7e805425930835cdeccccba
Daniel <dob9601@gmail.com>
2021-02-26 20:38:06 +0000

    Serialisation methods moved into schedule

--- schedule.py
+++ schedule.py
@@ -1,5 +1,7 @@
 from intersection import Intersection
 
+from __future__ import annotations
+
 
 class Schedule:
     def __init__(self, data: list[list[str]]):
@@ -7,3 +9,34 @@ class Schedule:
 
     def get_intersection_schedule(self, intersection: Intersection):
         return self.data[intersection.id]
+
+    def serialise(self, score: int, filename: str, solver_name: str) -> None:
+        filename = filename.split('.')[0]
+        with open(f'output/{filename}_{solver_name}_{score}.out', 'w') as file:
+            file.write(str(len(self.data)) + '\n')
+            for i, intersection in enumerate(self.data):
+                file.write(str(i) + '\n')
+                file.write(str(len(set(intersection))) + '\n')
+
+                current_street: str = intersection[0]
+                current_count = 1
+                for scheduled_street in intersection[1:]:
+                    if scheduled_street == current_street:
+                        current_count += 1
+                    else:
+                        file.write(f'{current_street} {str(current_count)}\n')
+                        current_street = scheduled_street
+                        current_count = 1
+                file.write(f'{current_street} {str(current_count)}\n')
+
+    @classmethod
+    def from_file(cls, filename: str) -> Schedule:
+        with open(f'ouput/{filename}.out','r') as file:
+            intersections = int(file.readline())
+            schedule = [[] for i in range(intersections)]
+            for intersection in range(intersections):
+                streets = int(file.readline())
+                for _ in range(streets):
+                    current_street = file.readline().split(" ")
+                    schedule[intersection] += [current_street[0]] * int(current_street[1])
+        return Schedule(schedule)
--- solutions/solution.py
+++ solutions/solution.py
@@ -12,37 +12,6 @@ class Solution(abc.ABC):
     def run(self) -> None:
         ...
 
-    @staticmethod
-    def serialise(self, score: int) -> None:
-        filename = self.filename.split('.')[0]
-        with open(f'output/{filename}_{self.name}_{score}.out', 'w') as file:
-            file.write(str(len(self.world.intersections)) + '\n')
-            for intersection in self.world.intersections:
-                file.write(str(intersection.id) + '\n')
-                file.write(str(len(set(intersection.schedule))) + '\n')
-
-                current_street: str = intersection.schedule[0]
-                current_count = 1
-                for scheduled_street in intersection.schedule[1:]:
-                    if scheduled_street == current_street:
-                        current_count += 1
-                    else:
-                        file.write(f'{current_street} {str(current_count)}\n')
-                        current_street = scheduled_street
-                        current_count = 1
-                file.write(f'{current_street} {str(current_count)}\n')
-
-    @staticmethod
-    def deserialise(filename: str) -> list[list[str]]:
-        with open(f'ouput/{filename}.out','r') as file:
-            intersections = int(file.readline())
-            schedule = [[] for i in range(intersections)]
-            for intersection in range(intersections):
-                streets = int(file.readline())
-                for _ in range(streets):
-                    current_street = file.readline().split(" ")
-                    schedule[intersection] += [current_street[0]] * int(current_street[1])
-        return schedule
 
     @property
     @abc.abstractmethod
